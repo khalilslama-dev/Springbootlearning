452073408d98b26aadbd507cee6f6df871b9e586
khalilslama-dev <slamaahmedkhalil@gmail.com>
2024-03-13 13:44:38 +0100

    Spring boot project init

---
+++ src/main/java/com/khalil/slama/dev/springboot/learning/springbootlearning/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.khalil.slama.dev.springboot.learning.springbootlearning;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringBootLearningApplication.class);
+    }
+
+}
